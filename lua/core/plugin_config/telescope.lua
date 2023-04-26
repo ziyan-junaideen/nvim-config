ed30ee2dddab4ecf6a3a599f6b8f97cf5613cc71
Ziyan Junaideen <ziyan@tryedge.com>
2023-04-26 22:47:15 +0530

    FZF with Telescope

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -9,6 +9,15 @@ require('telescope').setup {
       },
     },
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
 }
 
 -- Enable telescope fzf native, if installed
@@ -30,7 +39,7 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_branches, { desc = '[G]it [B]ranches' })
+vim.keymap.set('n', '<leader>sb', require('telescope.builtin').git_branches, { desc = '[G]it [B]ranches' })
 
 vim.keymap.set("n", "<leader>sr", require('telescope').extensions.frecency.frecency, { desc="[S]earch [R]ecent", noremap = true, silent = true})
 
