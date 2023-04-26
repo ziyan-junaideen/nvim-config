3bea3e0c123384d5cbe9b7da92bd8e563fd948c3
Ziyan Junaideen <ziyan@tryedge.com>
2023-04-26 19:08:31 +0530

    Copilot - notes

--- lua/core/plugin_config/copilot.lua
+++ lua/core/plugin_config/copilot.lua
@@ -2,3 +2,11 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
+
+
+--[[
+-- Resources: 
+-- https://www.reddit.com/r/neovim/comments/sk70rk/using_github_copilot_in_neovim_tab_map_has_been/ (tab disabled fix)
+-- https://github.com/orgs/community/discussions/11611 (copilot next previous mapping change)
+--
+--]]
