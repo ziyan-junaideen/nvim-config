12d30b51f02b5ce19a9a27ee3aa52fc83f27e0dc
Ziyan Junaideen <ziyan@tryedge.com>
2023-05-03 08:03:14 +0530

    CMP cleanup

--- lua/core/plugin_config/cmp.lua
+++ lua/core/plugin_config/cmp.lua
@@ -6,19 +6,19 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 local lspkind = require('lspkind')
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup({
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      mode = 'symbol',
+      maxwidth = 50,
+      ellipsis_char = '...',
       symbol_map = { Copilot = "" },
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        -- ...
-        return vim_item
-      end
     })
   },
   snippet = {
@@ -40,6 +40,14 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = false
     }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      else
+        fallback()
+      end
+    end),
+    --[[
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -59,6 +67,7 @@ cmp.setup({
         fallback()
       end
     end, {"i", "s"}),
+    --]]
   }),
   sources = cmp.config.sources({
     { name = 'copilot' },
