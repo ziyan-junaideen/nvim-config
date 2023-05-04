0f298645c257f3da3bcc782c537ad121f9e04f62
Ziyan Junaideen <ziyan@tryedge.com>
2023-05-04 08:55:04 +0530

    Add neodev

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -50,6 +50,7 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use "folke/neodev.nvim"
 
   -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
