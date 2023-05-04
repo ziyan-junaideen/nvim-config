3f57ebdf183466927c91a129e9a5da4fa4d172ce
Ziyan Junaideen <ziyan@tryedge.com>
2023-05-04 12:09:28 +0530

    Remove tabs

--- lua/core/plugin_config/lsp_zero.lua
+++ lua/core/plugin_config/lsp_zero.lua
@@ -33,13 +33,13 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers({
   function(server_name)
-	  local normal_capabilities = vim.lsp.protocol.make_client_capabilities()
-	  local capabilities = cmp_nvim_lsp.default_capabilities(normal_capabilities)
+    local normal_capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = cmp_nvim_lsp.default_capabilities(normal_capabilities)
 
-	  require("lspconfig")[server_name].setup {
-	    capabilities = capabilities,
-	    settings = servers[server_name]
-	  }
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+      settings = servers[server_name]
+    }
   end,
 })
 
@@ -73,7 +73,7 @@ lsp.setup_nvim_cmp({
         end,
       }
     }
-  },
+  }
 })
 
 lsp.setup() -- this needs to be last
