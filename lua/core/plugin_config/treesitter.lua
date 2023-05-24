621c2b0681fe7447dd2c8bdb72f80d47af8f29b8
Ziyan Junaideen <ziyan@jdeen.com>
2023-05-24 10:11:29 +0530

    Remove help from treesitter

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'python', 'rust', 'typescript', 'help', 'vim',
+  ensure_installed = { 'lua', 'python', 'rust', 'typescript', 'vim',
     'ruby', 'javascript', 'scss', 'css', 'elixir', 'erlang', 'dockerfile',
     'terraform', 'org' },
 
